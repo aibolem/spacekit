7f1a28d9adddbe70b55f3891ecf9cc2621faa6d0
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2023-09-24 11:46:16 +0200

    Update main.js

--- examples/asteroid_shape_from_earth/main.js
+++ examples/asteroid_shape_from_earth/main.js
@@ -11,7 +11,7 @@ const jdStart = jdTest;
 
 // Create the visualization and put it in our div.
 const viz = new Spacekit.Simulation(document.getElementById('main-container'), {
-  basePath: '../../src',
+  basePath: 'src',
   jd: jdStart,
   // startDate: Date.now(),
   startPaused: false,
