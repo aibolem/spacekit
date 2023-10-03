21c8be1f7d2206c3452dc9cd856d498968dce048
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2023-10-03 15:45:42 +0200

    Update mainII.js

--- examples/asteroid/mainII.js
+++ examples/asteroid/mainII.js
@@ -4,7 +4,7 @@ const viz = new Spacekit.Simulation(document.getElementById('main-container'), {
   startDate: Date.now(),
     camera: {
     initialPosition: [
- 74, 28, 0.7    ], 
+ 74, -28, 0.7    ], 
       enableDrift: true,
   },
 });
